33c35204107b5286c230efcd70f9b71a507f46fc
tostaylo <tostaylo1@gmail.com>
2017-03-07 16:30:02 -0800

    first commit

---
+++ redux-auth/src/rootReducer.js
@@ -0,0 +1,17 @@
+import { SET_CURRENT_USER } from './actions';
+
+const DEFAULT_STATE = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function (state = DEFAULT_STATE, action){
+  switch(action.type) {
+    case SET_CURRENT_USER:
+      return {
+        isAuthenticated: !!(Object.keys(action.user).length),
+        user: action.user
+      };
+    default: return state;
+  }
+}
