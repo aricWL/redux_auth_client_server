4dd662c11ba45b3e858170cb7f65dc34bde9a2da
Joel <japoncea@gmail.com>
2017-03-08 11:08:02 -0800

    made the 404 page the best

--- redux-auth/src/NotFound.js
+++ redux-auth/src/NotFound.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ReactPlayer from 'react-player'
 
 export default class NotFound extends Component {
   render() {
     return (
       <div>
         <h3>Oops! You are not allowed here!</h3>
-        <img src="https://httpstatusdogs.com/img/404.jpg" alt="404 not found"/>
+        <ReactPlayer url="https://www.youtube.com/watch?v=h_L4Rixya64" playing/>
         <p>Please find your way back to safety. In the meantime, please consider what the Foo Fighters ask of you.</p>
       </div>
     )
